09bfa4dc3c1e9d54545e260116c8d7b9ff200443
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2020-03-30 10:07:18 -0300

    Adicionando BackEnd

---
+++ Backend/tests/integration/ong.spec.js
@@ -0,0 +1,29 @@
+const request = require("supertest");
+const app = require("../../src/app");
+const connection = require("../../src/database/connection");
+
+describe("ONG", () => {
+  beforeEach(async () => {
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  it("should be able to create new ONG", async () => {
+    const response = await request(app)
+      .post("/ongs")
+      .send({
+        name: "APAD 4 teste",
+        email: "contato@apad.com",
+        whatsapp: "992203627",
+        city: "Taubaté",
+        uf: "SP"
+      });
+
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.id).toHaveLength(8);
+  });
+});
